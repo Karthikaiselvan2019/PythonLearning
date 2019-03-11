008c6c30dd22a520144bebfeacfcbc90ee2e0d19
Karthikaiselvan2019 <48044140+Karthikaiselvan2019@users.noreply.github.com>
2019-03-11 12:30:29 +0530

    Module and Packages, Decorators

---
+++ filetwo.py
@@ -0,0 +1,7 @@
+import fileone
+print("Top leven in filetwo.py")
+
+if __name__ == "__main__":
+    print("filetwo.py is being run directly")
+else:
+    print("filetwo.py is being imported into another")
---
+++ testcol.py
@@ -0,0 +1,6 @@
+
+#from colorama import init
+#init()
+from colorama import Fore
+print(Fore.BLUE + "Blue")
+print(Fore.RED  + "Red")
